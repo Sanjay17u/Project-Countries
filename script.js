887d73e1e58296d6b147a67ec97bba5e9d65af68
Sanjay Solanki <sanjay.solanki.17.om@gmail.com>
2025-02-26 21:01:00 +0530

    50% Work Done Fro Now, Move ON to the Final Part of The Project.

---
+++ script.js
@@ -0,0 +1,26 @@
+const countriesContainer = document.querySelector(".countries__container");
+
+fetch("https://restcountries.com/v3.1/all")
+  .then((res) => res.json())
+  .then((data) => {
+    data.forEach((country) => {
+        console.log(country)
+      const countryCard = document.createElement("a");
+      countryCard.classList.add("country__card");
+      countryCard.href = `/country.html?name=${country.name.common}`
+
+      const cardHTML = `
+            <img src="${country.flags.svg}" alt="flag__image">
+                <div class="card__text">
+                    <h3 class="card__title">${country.name.common}</h3>
+                    <p><b>Population: </b>${country.population.toLocaleString('en-IN')}</p>
+                    <p><b>Region: </b>${country.region}</p>
+                    <p><b>Capital: </b>${country.capital}</p>
+                </div>
+`;
+      countryCard.innerHTML = cardHTML;
+
+      countriesContainer.append(countryCard);
+    });
+  });
+
